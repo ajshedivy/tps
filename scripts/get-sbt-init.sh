5b0f6137e04f5c77d5e5e0764f5ea6c07b7a1846
Adam Shedivy <ajshedivy@wisc.edu>
2020-08-05 14:25:46 -0500

    fix

--- scripts/get-sbt-init.sh
+++ scripts/get-sbt-init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sbtscript=./sbt
+echo $0
+full_path=$(realpath $0)
+echo $full_path
 
 if [ ! -f $sbtscript ]; then
   echo "downloading sbt" 1>&2
-  ./get-sbt.sh
+  bash get-sbt.sh
 else 
     echo "scripts already downloaded"
 fi
