df1d23bfe5d89f402bcc6ce5713936bab50ab484
Adam Shedivy <ajshedivy@wisc.edu>
2020-08-05 14:32:18 -0500

    docker debugging

--- scripts/get-sbt-init.sh
+++ scripts/get-sbt-init.sh
@@ -3,11 +3,16 @@
 sbtscript=./sbt
 echo $0
 full_path=$(realpath $0)
-echo $full_path
+echo "full path:" $full_path
+
+work_dir=$(dirname $full_path)
+echo "dirname:" $work_dir
+
+echo $work_dir/get-sbt.sh
 
 if [ ! -f $sbtscript ]; then
   echo "downloading sbt" 1>&2
-  bash get-sbt.sh
+  bash $work_dir/get-sbt.sh
 else 
     echo "scripts already downloaded"
 fi
